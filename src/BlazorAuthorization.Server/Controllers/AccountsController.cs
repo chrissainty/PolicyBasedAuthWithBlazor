67ea6f425f8ad5dd6673473993502499f15f2e70
MarkStega <mark2002@stega.us>
2019-10-11 15:23:51 -0400

    2019-10-11 Cleanup or project names

---
+++ src/BlazorAuthorization.Server/Controllers/AccountsController.cs
@@ -0,0 +1,48 @@
+﻿using BlazorAuthorization.Shared;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAuthorization.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private static UserModel LoggedOutUser = new UserModel { IsAuthenticated = false };
+
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountsController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]RegisterModel model)
+        {
+            var newUser = new IdentityUser { UserName = model.Email, Email = model.Email };
+
+            var result = await _userManager.CreateAsync(newUser, model.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(x => x.Description);
+
+                return BadRequest(new RegisterResult { Successful = false, Errors = errors });
+            }
+
+            // Add all new users to the User role
+            await _userManager.AddToRoleAsync(newUser, "User");
+
+            // Add new users whose email starts with 'admin' to the Admin role
+            if (newUser.Email.StartsWith("admin"))
+            {
+                await _userManager.AddToRoleAsync(newUser, "Admin");
+            }
+
+            return Ok(new RegisterResult { Successful = true });
+        }
+    }
+}
