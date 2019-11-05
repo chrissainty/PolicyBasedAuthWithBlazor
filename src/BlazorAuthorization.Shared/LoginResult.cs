1b04446e40fc1825951ab9a72c04f14065587577
Chris Sainty <chris.sainty@flagship-group.co.uk>
2019-11-05 14:31:54 +0000

    Sorting messed up repo

--- src/BlazorAuthorization.Shared/LoginResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorAuthorization.Shared
-{
-    public class LoginResult
-    {
-        public bool Successful { get; set; }
-        public string Error { get; set; }
-        public string Token { get; set; }
-    }
-}
--- src/BlazorAuthorization.Shared/UserModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorAuthorization.Shared
-{
-    public class UserModel
-    {
-        public string Email { get; set; }
-        public bool IsAuthenticated { get; set; }
-    }
-}
