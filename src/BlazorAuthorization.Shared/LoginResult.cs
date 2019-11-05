b608b51fab7025b8ef75cd8e3cecc5f383bf44b9
Chris Sainty <chris.sainty@flagship-group.co.uk>
2019-11-05 16:12:26 +0000

    fix attempt

---
+++ src/BlazorAuthorization.Shared/LoginResult.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAuthorization.Shared
+{
+    public class LoginResult
+    {
+        public bool Successful { get; set; }
+        public string Error { get; set; }
+        public string Token { get; set; }
+    }
+}
